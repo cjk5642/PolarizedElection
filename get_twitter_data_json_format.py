c8832ba6faced30188d770db25af739e2b85d7a9
Tom <tommyzhao111@gmail.com>
2020-11-02 17:18:45 -0500

    update scraper

--- get_twitter_data_json_format.py
+++ get_twitter_data_json_format.py
@@ -4,11 +4,11 @@ from tweepy import OAuthHandler
 import json
 import tweepy
 
-election2020_filter = "#trump OR #biden OR #election OR #election2020"
-date_since = "2020-10-01"
-output_filename = 'election_filter1.jsonl'
 
-def scrape_tweets(search_words, date_since, num_tweets = 5000000):
+output_filename = 'election_filter_10-26_10-29.jsonl'
+
+t = 0
+def scrape_tweets(search_words, date_since, date_until, num_tweets = 5000000):
     ''' Scrapes tweets until the date_since date is reached or num_tweets is reached '''
     
     creds = {'CONSUMER_KEY': "XtMJv3wN6AJqhuwYtIYJavPOP", 
@@ -22,14 +22,11 @@ def scrape_tweets(search_words, date_since, num_tweets = 5000000):
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     
     # Collect tweets using the Cursor object
-    tweets = tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since, tweet_mode='extended').items()
+    tweets = tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since, until=date_until, tweet_mode='extended').items()
     
     count = 0
     for tweet in tweets:
         count += 1
-        if tweet.retweeted: #skip retweets
-            print("skipping retweet")
-            continue
         dict_to_append = {}
         dict_to_append['username'] = tweet.user.screen_name
         dict_to_append['acct_desc'] = tweet.user.description
@@ -42,9 +39,9 @@ def scrape_tweets(search_words, date_since, num_tweets = 5000000):
         dict_to_append['retweet_count'] = tweet.retweet_count
         dict_to_append['hashtags'] = tweet.entities['hashtags']
         dict_to_append['text'] = tweet.full_text
+        dict_to_append['favorite_count'] = tweet.favorite_count
         
-        
-        print(f"Tweet #{count}\tDate: {dict_to_append['tweet_created_ts']}")
+        if count % 10 == 0: print(f"Tweet #{count}\tDate: {dict_to_append['tweet_created_ts']}")
         
         with open(output_filename, "a") as outfile:  
             json.dump(dict_to_append, outfile) 
@@ -52,4 +49,7 @@ def scrape_tweets(search_words, date_since, num_tweets = 5000000):
         
         if count > num_tweets: break
     
-scrape_tweets(election2020_filter, date_since)
+election2020_filter = "#trump OR #biden OR #election OR #election2020 -filter:retweets"
+date_since = "2020-10-26"
+date_until = "2020-10-29"
+scrape_tweets(election2020_filter, date_since, date_until)
